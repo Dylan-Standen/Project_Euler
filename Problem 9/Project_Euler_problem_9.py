5f754e12cb155f857e0ea88d997a6a9d425b82f6
Dylan-Standen <dylansion38@gmail.com>
2026-01-19 22:07:28 +0000

    Add files via upload

---
+++ Problem 9/Project_Euler_problem_9.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 19 12:36:18 2026
+
+@author: dylanstanden
+
+"""
+def triple(n) -> int:
+    
+  
+   for c in range(1,n):
+     for b in range(1,c):
+        for a in range(1,b):
+            if a**2 + b**2 == c**2 and a + b + c == n:
+                return a*b*c, (a,b,c)
+                
+    
+    
+        
+        
+print(triple(1000))
